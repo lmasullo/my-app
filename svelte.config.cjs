528c26d3467b2ebf2536f08d6e2d127797f03fe9
Larry Masullo <lmasullo@gmail.com>
2021-04-28 15:01:34 -0500

    added toml

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,7 +1,10 @@
+const netlify = require('@sveltejs/adapter-netlify');
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: netlify(),
 	}
 };
